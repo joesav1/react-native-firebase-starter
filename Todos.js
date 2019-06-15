6ab0f522df67d65c727606e8309bf0754e89cb72
joesav1 <joe.m.savidge@gmail.com>
2019-06-15 12:31:01 +0100

    made some files

---
+++ Todos.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import firebase from 'react-native-firebase';
+import { ScrollView, View, Text, TextInput, Button } from 'react-native';
+
+class Todos extends React.Component {
+    constructor() {
+        super();
+        this.ref = firebase.firestore().collection('todos');
+        this.unsubscribe = null;
+
+        this.state = {
+            TextInput: '',
+            loading: true,
+            todos: [],
+        };
+    }
+}
+
+
+
+export default Todos;
