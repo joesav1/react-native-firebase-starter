0a140ac6f2aed4d3efd649f199696724eb06959e
joesav1 <joe.m.savidge@gmail.com>
2019-06-17 10:48:14 +0100

    added the todo pure component

--- App.js
+++ App.js
@@ -6,65 +6,20 @@ import Todos from './Todos';
 import firebase from 'react-native-firebase';
 
 
+AppRegistry.registerComponent('apptesttwo', () => Todos);
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.ref = firebase.firestore().collection('todos');
-    this.state = {
-      TextInput: '',
-    };
-  }
-  updateTextInput(value) {
-    this.setState({ TextInput: value })
+  render(){
+    console.log("JS - testing tingz");
+    return(
+      <Todos/>
+    );
+    
   }
 
-  addTodo() {
-    this.ref.add({
-      title: this.state.TextInput,
-      complete: false,
-    });
 
-    this.setState({
-      TextInput: '',
-    });
-  }
 
-  render() {
-    return (             <View>
-      <ScrollView>
-          <Text> List of TODOs</Text>
-      </ScrollView>
-      <TextInput
-          placeholder={'Add TODO'}
-          value={this.state.TextInput}
-          onChangeText={(text) => this.updateTextInput(text)}
-      />
-      <Button
-          title={'Add TODO'}
-          disabled={!this.state.TextInput.length}
-          onPress={()=>{this.addTodo()}}
-          // disabled = {true}
-          // onPress={()=>{}}
-
-      />
-  </View>);
-  }
-
-  // async componentDidMount() {
-  //   // TODO: You: Do firebase things
-  //   // const { user } = await firebase.auth().signInAnonymously();
-  //   // console.warn('User -> ', user.toJSON());
-
-  //   // await firebase.analytics().logEvent('foo', { bar: '123'});
-  // }
 
 }
-
-
-AppRegistry.registerComponent('apptesttwo', () => Todos);
-
-
-
 
 
 
--- Todos.js
+++ Todos.js
@@ -1,25 +1,102 @@
 import React from 'react';
 import firebase from 'react-native-firebase';
-import { ScrollView, View, Text, TextInput, Button } from 'react-native';
-
+import { Flatlist, ScrollView, View, Text, TextInput, Button } from 'react-native';
+import Todo from './Todo';
 class Todos extends React.Component {
     constructor() {
         super();
         this.ref = firebase.firestore().collection('todos');
-        // this.unsubscribe = null;
+        this.unsubscribe = null;
 
-        // this.state = {
-        //     TextInput: '',
-        //     loading: true,
-        //     todos: [],
-        // };
-    }
-    render() {
-        return null;
-    }
+        this.state = {
+          TextInput: '',
+          loading: true,
+          todos: [],
+        };
+      }
 
+      componentDidMount() {
+          this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
+      }
+
+      componentWillUnmount() {
+          this.unsubscribe();
+      }
+
+      onCollectionUpdate = (querySnapshot) => {
+          const todos = [];
+          querySnapshot.forEach((doc) => {
+              const {title, complete} = doc.data();
+
+              todos.push({
+                  key: doc.id,
+                  doc,
+                  title,
+                  complete,
+              });
+          });
+
+          this.setState({
+              todos,
+              loading: false,
+          });
+      }
+
+
+      updateTextInput(value) {
+        this.setState({ TextInput: value })
+      }
+    
+      addTodo() {
+        this.ref.add({
+          title: this.state.TextInput,
+          complete: false,
+        });
+    
+        this.setState({
+          TextInput: '',
+        });
+      }
+    
+      render() {
+        if(this.state.loading) {
+            return null;
+        }
+
+        return (
+            
+        <View style = {{ flex: 1}}>
+          <Flatlist
+            data={this.state.todos}
+            renderItem={({item}) => <Todo {...item}/>}
+            />
+          <TextInput
+              placeholder={'Add TODO'}
+              value={this.state.TextInput}
+              onChangeText={(text) => this.updateTextInput(text)}
+          />
+          <Button
+              title={'Add TODO'}
+              disabled={!this.state.TextInput.length}
+              onPress={()=>{this.addTodo()}}
+                            
+              // disabled = {true}
+              // onPress={()=>{}}
+    
+          />
+        </View>);
+      }
+    
+      // async componentDidMount() {
+      //   // TODO: You: Do firebase things
+      //   // const { user } = await firebase.auth().signInAnonymously();
+      //   // console.warn('User -> ', user.toJSON());
+    
+      //   // await firebase.analytics().logEvent('foo', { bar: '123'});
+      // }
+    
 }
-
+    
 
 
 export default Todos;
